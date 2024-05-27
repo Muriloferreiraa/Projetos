ef0b428c03e616b5e8c17d33c663f0608af3cf9f
MuriloFerreiraa <muriloferrei.ra@outlook.com>
2024-05-27 12:31:07 -0300

    [feat] Java
    Aprendendo Java

---
+++ bootcamp java/operadores/opredorTernario.java
@@ -0,0 +1,17 @@
+public class opredorTernario {
+    public static void main(String[] args) {
+        int a,b;
+        a=5;
+        b=6;
+        String resultado = a==b ? "verdadeiro":"falso";
+        if(a==b)
+        System.out.println("igual");
+        else if (a<b)
+        System.out.println("certo");
+        else
+        System.out.println("errado");
+
+        System.out.println(resultado);
+
+    }
+}
